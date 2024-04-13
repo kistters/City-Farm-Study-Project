0e5fd9ce00bd7d5ac6ccee2a3f074e2b45ada72e
Rafael Kisters Ventura da Silva <kistters@gmail.com>
2024-04-13 14:27:33 -0300

    removing vue frontend - dedicated to use test and python backend

--- frontend-vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import axiosPlugin from './plugins/axios';
-import router from "@/routes";
-
-createApp(App)
-    .use(axiosPlugin)
-    .use(router)
-    .mount('#app')

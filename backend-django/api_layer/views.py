347ce58746a4c01743c817c713a4ed2c3abeef64
Rafael Kisters Ventura da Silva <kistters@gmail.com>
2024-04-17 15:41:49 -0300

    small fix

--- backend-django/api_layer/views.py
+++ backend-django/api_layer/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
@@ -28,6 +29,7 @@ class ObtainAuthTokenView(ObtainAuthToken):
 
 class LogoutView(APIView):
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         request.user.auth_token.delete()

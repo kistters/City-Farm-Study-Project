92a0e50aa1f7b357200aa4f4493208892c27f111
Rafael Kisters Ventura da Silva <kistters@gmail.com>
2024-04-17 15:37:42 -0300

    optimize implementation of ObtainAuthToken

--- backend-django/api_layer/urls.py
+++ backend-django/api_layer/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from api_layer.views import (
-    LogoutView, LoginView, RegisterView,
+    LogoutView, ObtainAuthTokenView, RegisterView,
 )
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('obtain-auth-token/', ObtainAuthTokenView.as_view(), name='obtain-auth-token'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- backend-django/api_layer/views.py
+++ backend-django/api_layer/views.py
@@ -1,11 +1,10 @@
-from django.contrib.auth import authenticate
-
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from api_layer.serializers import (
     UserSerializer,
@@ -16,22 +15,20 @@ class RegisterView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+class ObtainAuthTokenView(ObtainAuthToken):
+    renderer_classes = APIView.renderer_classes
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
 
 
 class LogoutView(APIView):
     authentication_classes = [TokenAuthentication]
 
     def post(self, request):
-        if request.user.is_authenticated:
-            request.user.auth_token.delete()
-        return Response(status=204)
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
